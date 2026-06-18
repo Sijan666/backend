0c7852c281d35ff4594f65aea12cca8f25a5e8d7
Sijan666 <666majharulislam@gmail.com>
2026-06-19 01:50:37 +0600

    update controller

--- controller/registrationController.js
+++ controller/registrationController.js
@@ -1,39 +1,39 @@
 const User = require('../model/usermodel')
 
-// const registrationController = async (req,res)=>{
-//     const { username , email , pass} = req.body
-//     const existingEmail = await User.findOne({email:email})
+const registrationController = async (req,res)=>{
+    const { username , email , pass} = req.body
+    const existingEmail = await User.findOne({email:email})
     
-//     if (existingEmail) {
-//         return res.send('user already exist')
-//     }
+    if (existingEmail) {
+        return res.send('user already exist')
+    }
 
-//     const user = new User({
-//         username : username,
-//         email : email , 
-//         pass : pass
-//     })
+    const user = new User({
+        username : username,
+        email : email , 
+        pass : pass
+    })
 
-//     user.save()
-//     res.send(user)
-// }
+    user.save()
+    res.send(user)
+}
 
-// const allUserController = async (req,res) => {
-//     const allUser = await User.find({})
-//     res.send(allUser)
-// }
+const allUserController = async (req,res) => {
+    const allUser = await User.find({})
+    res.send(allUser)
+}
 
-// const deleteUser = async (req,res) => {
-//     const {id} = req.params
-//     await User.findByIdAndDelete(id)
-//     res.send('user deleted')
-// }
+const deleteUser = async (req,res) => {
+    const {id} = req.params
+    await User.findByIdAndDelete(id)
+    res.send('user deleted')
+}
 
-// const updateController = async (req,res) => {
-//     const {id} = req.params
-//     let data = await User.findByIdAndUpdate({_id:id} , req.body)
-//     res.send('data updated')
-// }
+const updateController = async (req,res) => {
+    const {id} = req.params
+    let data = await User.findByIdAndUpdate({_id:id} , req.body)
+    res.send('data updated')
+}
 
 // practice
 // const reg = async (req,res) => {
